1c2ed8f644a6a26bbe56241bb29c3f525da5587d
Your Name <you@example.com>
2021-02-09 21:14:58 +0900

    トップページ作成、ユーザ登録機能作成

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateUsersTable extends Migration
 {
@@ -15,10 +16,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable;
             $table->rememberToken();
             $table->timestamps();
         });
