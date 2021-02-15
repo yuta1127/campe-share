fa06c8c6d2a9c9c0c23c76848a03202173aaac4b
Your Name <you@example.com>
2021-02-15 22:33:46 +0900

    トップページにも検索機能のロジックを追加

--- laravel/app/Http/Controllers/TopController.php
+++ laravel/app/Http/Controllers/TopController.php
@@ -7,14 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class TopController extends Controller
 {
-    public function top()
+    public function top(Request $request)
     {
-        {
-            $articles = DB::table('articles')
-            ->select('title','content','created_at')
-            ->get();
+        $search = $request->input('search');
 
-            return view('top',compact('articles'));
+        //検索フォーム用
+        $query = DB::table('articles');
+ 
+        if($search !== null){
+            //全角スペースを半角に
+            $search_split = mb_convert_kana($search,'s');
+ 
+            //空白で区切る
+            $search_split2 = preg_split('/[\s]+/',$search_split,-1,PREG_SPLIT_NO_EMPTY);
+ 
+            //単語をループで回す
+            foreach($search_split2 as $value)
+            {
+                $query->where('title','like','%'.$value.'%');
+            }
+ 
         }
+
+        $articles = $query->select('id','title','content','created_at')
+        ->get();
+            return view('top',compact('articles'));
     }
 }
