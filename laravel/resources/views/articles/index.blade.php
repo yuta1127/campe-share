3b582a03c381bdc53c077196d4837f1e78ca1d37
Your Name <you@example.com>
2021-02-17 06:24:56 +0900

    いいね機能

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -39,14 +39,17 @@
             {{ $article->created_at }}
           </div>
         </div>
-        <div class="card-body pt-0 pb-2 pl-3">
-    <div class="card-text">
-      <article-like
-        :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'      
-      >
-      </article-like>
+    <div class="card-body pt-0 pb-2 pl-3">
+      <div class="card-text">
+       <article-like
+        :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+        :initial-count-likes='@json($article->count_likes)'
+        :authorized='@json(Auth::check())'
+        endpoint="{{ route('articles.like', ['article' => $article]) }}"
+        >
+       </article-like>
+     </div>
     </div>
-  </div>
    </div>
    @endforeach
 @endsection
