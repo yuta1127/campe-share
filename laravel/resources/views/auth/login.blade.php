527b5deeff95ad449a39fa44de2102357394970d
Your Name <you@example.com>
2021-02-26 22:57:35 +0900

    レイアウト修正とメッセージなどの日本語化

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('app')
 
 @section('content')
+@include('nav')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
