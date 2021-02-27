2e25bc0a92318f33375c19699087ce3329cdcac1
Your Name <you@example.com>
2021-02-27 22:52:45 +0900

    ナビゲーションバーのログアウト機能を追加

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -15,24 +15,5 @@
     <li class="nav-item">
       <a class="nav-link" href="{{ route('articles.index') }}">ゲストログイン</a>
     </li>
-
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-user-circle"></i>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
-    </li>
-    <form id="logout-button" method="POST" action="">
-    </form>
-    <!-- Dropdown -->
-
   </ul>
-
 </nav>
