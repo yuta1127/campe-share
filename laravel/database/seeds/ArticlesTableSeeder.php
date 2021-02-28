e451e272181a6fb81b5034dd8ab03327897fe3df
Your Name <you@example.com>
2021-02-28 20:11:55 +0900

    articlesテーブルのseedデータを変更

--- laravel/database/seeds/ArticlesTableSeeder.php
+++ laravel/database/seeds/ArticlesTableSeeder.php
@@ -12,72 +12,72 @@ class ArticlesTableSeeder extends Seeder
     public function run()
     {
         DB::table('articles')->insert([
-            ['user_id' => '22',
+            ['user_id' => '1',
             'title' => 'php',
             'content' => 'テスト5',
             'created_at' => '2021-02-10 22:25:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '2',
             'title' => 'docker',
             'content' => 'テスト6',
             'created_at' => '2021-02-10 22:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '3',
             'title' => 'HTML',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '4',
             'title' => 'CSS',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '5',
             'title' => 'javascript',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '6',
             'title' => 'Ruby',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '7',
             'title' => 'Java',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '8',
             'title' => 'Python',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '9',
             'title' => 'Go',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '10',
             'title' => 'C',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '1',
             'title' => 'swift',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '12',
             'title' => 'kotlin',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '13',
             'title' => 'AWS',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '22',
+            ['user_id' => '14',
             'title' => 'GCP',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
@@ -107,12 +107,12 @@ class ArticlesTableSeeder extends Seeder
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '20',
+            ['user_id' => '1',
             'title' => 'PostgreSQL',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
             ],
-            ['user_id' => '21',
+            ['user_id' => '1',
             'title' => 'Ruby on Rails',
             'content' => 'テスト3',
             'created_at' => '2021-02-10 23:32:06'
